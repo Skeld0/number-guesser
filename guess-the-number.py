a05167917c8fc6907d6301ca09b0eef09cac67d8
Skeld0 <216222261+Skeld0@users.noreply.github.com>
2025-09-30 00:30:04 +0300

    Initial commit: add number guessing game script

---
+++ guess-the-number.py
@@ -0,0 +1,23 @@
+import random
+def guesser():
+    right_number = random.randint(0,10)
+    atempts = 0
+    print("Ошибка! Введено не число. ")
+    while True:
+        try: 
+            user_number = int(input("Try to guess number from 0 to 10: "))
+        except ValueError:
+            print("Error. You need to write number")
+            continue    
+        if user_number < right_number:
+            user_number = int(input("Too small 😢. Try again! "))
+            
+            atempts +=1
+        elif user_number > right_number:
+            user_number = int(input("Too big 😢. Try again! "))
+            atempts +=1
+        else:
+            print(f"You are right! 🎉🥳 The answer is {right_number} and you did it in {atempts+1} atempts")
+            break
+if __name__ == "__main__":
+    guesser()
